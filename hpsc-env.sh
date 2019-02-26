8d327a69b6104986fc7ceed60f710e2deec7caae
Alexei Colin <acolin@isi.edu>
2019-02-26 07:26:04 -0800

    env script for HPSC SW on SCS Zebu server

---
+++ hpsc-env.sh
@@ -0,0 +1,15 @@
+ROOT=/projects/boeing/isi
+export HPSC_ROOT=$ROOT/hpsc-root
+HPREFIX=$ROOT/usr
+POKY=$ROOT/opt/poky/2.4.3/sysroots/x86_64-pokysdk-linux/usr/bin/aarch64-poky-linux
+
+export PYTHONPATH=$PYTHONPATH:$HPREFIX/lib/python2.7/site-packages
+
+PATH=$PATH:$ROOT/usr/sbin:$ROOT/usr/bin
+PATH=$PATH:$ROOT/opt/gcc-arm-none-eabi-7-2018-q2-update/bin
+PATH=$PATH:$POKY
+PATH=$PATH:$HPSC_ROOT/u-boot-a53/tools
+PATH=$PATH:$HPSC_ROOT/hpsc-bsp
+export PATH
+
+export LD_LIBRARY_PATH=$ROOT/usr/lib64:$ROOT/usr/lib
