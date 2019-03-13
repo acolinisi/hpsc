a92c418525c9c021aefee86b49468dc22fb5386e
Alexei Colin <acolin@isi.edu>
2019-03-13 16:22:39 -0700

    env: add ARM Aarch64 Cortex-A toolchain to PATH

--- hpsc-env.sh
+++ hpsc-env.sh
@@ -1,12 +1,14 @@
 ROOT=/projects/boeing/isi
 export HPSC_ROOT=$ROOT/hpsc
 HPREFIX=$ROOT/usr
+OPT=$ROOT/opt
 POKY=$ROOT/opt/poky/2.4.3/sysroots/x86_64-pokysdk-linux/usr/bin/aarch64-poky-linux
 
 export PYTHONPATH=$PYTHONPATH:$HPREFIX/lib/python2.7/site-packages
 
 PATH=$PATH:$HPREFIX/sbin:$HPREFIX/bin
-PATH=$PATH:$ROOT/opt/gcc-arm-none-eabi-7-2018-q2-update/bin
+PATH=$PATH:$OPT/gcc-arm-none-eabi-7-2018-q2-update/bin
+PATH=$PATH:$OPT/gcc-arm-8.2-2019.01-x86_64-aarch64-linux-gnu/bin
 PATH=$PATH:$POKY
 PATH=$PATH:$HPSC_ROOT/hpps/u-boot/tools
 PATH=$PATH:$HPSC_ROOT/hpsc-utils/host
