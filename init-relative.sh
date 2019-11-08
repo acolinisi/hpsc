ce2d5f560877d81e4501f4032b99b3928f2f7d1f
Alexei Colin <acolin@isi.edu>
2019-11-08 15:56:41 -0800

    init-relative: set URLs at top level too

--- init-relative.sh
+++ init-relative.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-# Initialize the doubly-nested submodules with URLs overriden
-# to subpaths relative to the path of the origin remote.
+# Initialize the submodules with URLs overriden to subpaths relative to the
+# path of the origin remote.
 #
 # This is useful for servers that are offline (no Internet connectivity),
 # and that thus have all submodule repositories available at the
 # same path as the parent repository.
+#
+# NOTE: this script is not recursive, it initializes URLs for submodules nested
+# only up to depth = 1 (i.e. top-level plus one more level).
 
 # Make command failures fatal
 set -e
-
-# Top-level submodules that contain nested submodules
-# NOTE: this script is not recursive, it handles only depth = 1
-MODS=(
-    sdk/qemu
-)
 
 SELF_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"
 source ${SELF_DIR}/git-alias.sh
@@ -41,10 +38,13 @@ echo "Initializing submodule URLs relative: $BASE_URL"
 # Print commands
 set -x
 
-git submodule update --init -- ${MODS[@]}
+declare -a "mods=($(git submodule | sed -n 's/^.[A-Fa-f0-9]\+\s\+\(\S\+\).*/\1/p'))"
 
-for mod in ${MODS}
+for mod in ${mods[@]}
 do
+    git config submodule.${mod}.url ${BASE_URL}/${mod}
+    git submodule update --init -- ${mod}
+
     pushd ${mod}
     declare -a "nested_mods=($(git submodule | sed -n 's/^.[A-Fa-f0-9]\+\s\+\(\S\+\).*/\1/p'))"
     for nested_mod in ${nested_mods[@]}
