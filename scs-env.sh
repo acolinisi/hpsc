9fbe4c37aa0bcdcebc0b47ec7327e2a904135361
Alexei Colin <acolin@isi.edu>
2019-11-08 14:05:25 -0800

    scs-env: fetchcache setup moved into master branch

--- scs-env.sh
+++ scs-env.sh
@@ -15,30 +15,5 @@ git() {
 # Set this to a least 8, but don't hog shared resources.
 alias make="make -j16"
 
-# "Upstream" clone of the HPSC repository to which multiple users push/pull
-HPSC_ROOT=/projects/boeing/isi/hpsc
-
-# Since the `scsrt` server is offline, set a variable to point to a directory
-# with source tarballs that are components of the HPSC SDK.
-#
-# During build of the HPSC SDK, when the following variable is set, in the
-# environment or on the make command line, source taballs are fetched (copied)
-# from there instead of from the Internet.
-#
-# This directory already exists with all the prefetched sources on the `scsrt`
-# server, but in case you want to create it on an online machine, run `make
-# sdk-fetch` and the directory will be populated at `sdk/bld/fetch`.
-export FETCH_CACHE=$HPSC_ROOT/sdk/bld/fetch
-
-# Similarly, during build of the Yocto root files (for configuration profiles
-# that use it), the directory where bitbake looks fro source tarballs and
-# repository clones is overriden by the following variable.
-#
-# This directory already exists with all the prefetched sources on the `scsrt`
-# server, but in case you want to create it in a working copy on an online
-# machine, run `make prof/lib-hpps-yocto/bld/hpps/yocto/fetch` and the
-# directory will be populated at `prof/lib-hpps-yocto/bld/hpps/yocto/poky_dl`
-# by default, or at the location pointed to by the YOCTO_DL_DIR variable if you
-# overrode it in the environment or on the make command line:
-
-export YOCTO_DL_DIR=$HPSC_ROOT/ssw/hpps/yocto/poky_dl
+SELF_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"
+source ${SELF_DIR}/fetchcache-relative.sh
